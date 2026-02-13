2cbdb6ea94e9a5b84dff95ed4c18af4a299cf623
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2026-02-13 16:04:32 +0100

    deleting a reservation functionality

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
-
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -18,15 +18,35 @@ export async function updateGuest(formData) {
 
   const updateData = { nationality, countryFlag, nationalID };
 
-  const { data, error } = await supabase
+  const { error } = await supabase
     .from("guests")
     .update(updateData)
-    .eq("id", session.user.guestId)
-    
+    .eq("id", session.user.guestId);
+
   if (error) {
     throw new Error("Guest could not be updated");
   }
-  revalidatePath("/account/profile")
+  revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsId = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsId.includes(bookingId))
+    throw new Error("You can't delete a booking that does not belong to you");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+  if (error) {
+    throw new Error("Reservation could not be deleted could not be deleted");
+  }
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
